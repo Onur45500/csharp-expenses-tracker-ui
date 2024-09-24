22d53cd08dd2bb0472285e02ceab0716ba6fc7dd
Onur45500 <onur.akmese@viacesi.fr>
2024-09-24 10:34:03 +0200

    Reinitialized the project

---
+++ csharp-expenses-tracker-ui/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace csharp_expenses_tracker_ui.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
